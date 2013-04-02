db15c89d0603cea0e1bc565acc8e6b76ad7a76b5
KILL3RTACO <kill3rtaco@gmail.com>
2013-04-01 17:33:35 -0700

    Enable and disable messages

--- src/com/kill3rtaco/partychat/PartyChat.java
+++ src/com/kill3rtaco/partychat/PartyChat.java
@@ -6,12 +6,12 @@ public class PartyChat extends TacoPlugin {
 
 	@Override
 	public void onStart() {
-		
+		chat.out("Enabled");
 	}
 
 	@Override
 	public void onStop() {
-		
+		chat.out("Disabled");
 	}
 
 }
